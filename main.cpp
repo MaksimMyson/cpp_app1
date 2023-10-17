d7c86ed1252fb68bfda9a36801b951f9fce4bd47
MaksimMyson <maksimmyson33@gmail.com>
2023-10-17 23:56:58 +0300

    ex1

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
+		std::cout << "Когда,стройна и светлоока," << std::endl;
+		std::cout << "Передо мной стоит оона ..." << std::endl;
+		std::cout << "Я мыслю: как же я счастлив!" << std::endl;
+		std::cout << "Она была разведена !" << std::endl;
 	return 0;
 }

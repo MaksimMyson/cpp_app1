cc977f1c3d7804b2a75f690ba9cdba6f9f6878b8
MaksimMyson <maksimmyson33@gmail.com>
2023-10-18 00:14:26 +0300

    ex2

--- main.cpp
+++ main.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
+	    std::cout << \t\t\t Ewery hunter whants to know" << std::endl;
+		std::cout << \t\t\t "Where does the pheasant sit?" << std::endl;
+		std::cout << \t\t\t "Where does the pheasant sit?" << std::endl;
+		std::cout << \t\t\t "In the forest, in the forest." << std::endl;
+
+		
+			
 	return 0;
 }

d5847441ca57b83bf263ef93054173d69a7ad65c
MaksimMyson <maksimmyson33@gmail.com>
2023-10-20 16:02:45 +0300

    ex3

--- main.cpp
+++ main.cpp
@@ -2,5 +2,95 @@
 
 int main()
 {
+
+
+	
+
+	std::cout << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187) << std::endl;
+	
+	std::cout << char(186) << "Number:" << char(186) << "222-222-222" << char(186) << std::endl;
+	
+	std::cout << char(204) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(206) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(185) << std::endl;
+	
+	std::cout << char(186) << "  222  " << char(186) << "     222   " << char(186) << std::endl;
+	
+	std::cout << char(204) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(206) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(185) << std::endl;
+	
+	std::cout << char(186) << "   22  " << char(186) << "     22    " << char(186) << std::endl;
+	
+	std::cout << char(204) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(206) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(185) << std::endl;
+	
+	std::cout << char(186) << "    2  " << char(186) << "      2    " << char(186) << std::endl;
+	
+	std::cout << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(202) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(188) << std::endl;
+
+
 	return 0;
 }
+
+
+
+//#include <iostream>
+
+//int main()
+{
+
+
+	//COORD pos = { 50, 1 };
+	//HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+	//SetConsoleCursorPosition(output, pos);
+	//SetConsoleTextAttribute(output, 11);
+
+	//std::cout << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187) << std::endl;
+	//pos.Y = 2;
+	//SetConsoleCursorPosition(output, pos);
+	//std::cout << char(186) << "Number:" << char(186) << "222-222-222" << char(186) << std::endl;
+	//pos.Y = 3;
+	//SetConsoleCursorPosition(output, pos);
+	//std::cout << char(204) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(206) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(185) << std::endl;
+	//pos.Y = 2;
+	//SetConsoleCursorPosition(output, pos);
+	//std::cout << char(186);
+	// SetConsoleTextAttribute(output, 12);
+	//std::cout << "  222  ";
+	//SetConsoleTextAttribute(output, 10);
+	//std::cout << char(186);
+	// SetConsoleTextAttribute(output, 12);
+	//std::cout << "     222   ";
+	// SetConsoleTextAttribute(output, 11);
+	//std::cout << char(186) << std::endl;
+	//pos.Y = 3;
+	//SetConsoleCursorPosition(output, pos);
+	//std::cout << char(204) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(206) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(185) << std::endl;
+	//pos.Y = 2;
+	//SetConsoleCursorPosition(output, pos);
+	//std::cout << char(186);
+	// SetConsoleTextAttribute(output, 12);
+	//std::cout << "   22  ";
+	// SetConsoleTextAttribute(output, 11);
+	//std::cout << char(186);
+	// SetConsoleTextAttribute(output, 12);
+	//std::cout << "     22    ";
+	// SetConsoleTextAttribute(output, 11);
+	//std::cout << char(186) << std::endl;
+	//pos.Y = 3;
+	//SetConsoleCursorPosition(output, pos);
+	//std::cout << char(204) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(206) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(185) << std::endl;
+	//pos.Y = 2;
+	//SetConsoleCursorPosition(output, pos);
+	//std::cout << char(186);
+	// SetConsoleTextAttribute(output, 12);
+	//std::cout << "    2  ";
+	// SetConsoleTextAttribute(output, 11);
+	//std::cout << char(186);
+	// SetConsoleTextAttribute(output, 12);
+	//std::cout << "      2    ";
+	// SetConsoleTextAttribute(output, 11);
+	//std::cout << char(186) << std::endl;
+	//pos.Y = 3;
+	//std::cout << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(202) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(188) << std::endl;
+
+
+	//return 0;
+
+//} 
